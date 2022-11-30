b9375a5ebb0347ed91fb246db43c1cc54deb2a7f
Yilong Zhang <elong0527@gmail.com>
2022-11-30 17:13:52 +0000

    add wlr_weight_mb following  Magirr (2021)

--- R/wlr_weight.R
+++ R/wlr_weight.R
@@ -20,7 +20,8 @@
 #' * `wlr_weight_fh` is Fleming-Harriongton, FH(rho, gamma) weight function.
 #' * `wlr_weight_1`  is constant for log rank test
 #' * `wlr_weight_power` is Gehan-Breslow and Tarone-Ware weight function.
-#'
+#' * `wlr_weight_mb` is  Magirr (2021) weight function.
+#' 
 #' @param x analysis time
 #' @param arm0 an "arm" object defined in `npsurvSS` package
 #' @param arm1 an "arm" object defined in `npsurvSS` package
@@ -77,4 +78,12 @@ wlr_weight_n <- function(x, arm0, arm1, power = 1){
   tmax <- arm0$total_time
   
   (n * (p0 * prob_risk(arm0, x, tmax) + p1 * prob_risk(arm1, x,tmax)))^power
+}
+
+#' @rdname wlr_weight
+#' @export
+wlr_weight_mb <- function(x, arm0, arm1, tau = NULL, wmax = Inf){
+  
+  pmin(wmax, wlr_weight_fh(x, arm0, arm1, rho = -1, gamma = 0, tau = tau))
+  
 }
