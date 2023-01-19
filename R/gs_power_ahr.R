6a16bf3e6ca0f60eed2ef2ad8e4419e17b9215dc
Zhao, Yujie <yujie.zhao@merck.com>
2023-01-19 08:57:01 -0500

    fix lpar bug

--- R/gs_power_ahr.R
+++ R/gs_power_ahr.R
@@ -180,6 +180,14 @@ gs_power_ahr <- function(enroll_rate = tibble(
   }
 
   # Check if it is two-sided design or not
+  if(all(test_lower) == FALSE){
+    lpar = rep(-Inf, K)
+  }
+  
+  if(all(test_upper) == FALSE){
+    upar = rep(Inf, K)
+  }
+  
   if (identical(lower, gs_b) & (!is.list(lpar))) {
     two_sided <- ifelse(identical(lpar, rep(-Inf, K)), FALSE, TRUE)
   } else {
