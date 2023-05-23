76902047da123f93e856313b69c3c29b99b863fa
Nan Xiao <me@nanx.me>
2023-05-23 19:48:19 -0400

    Organize Rcpp functions across multiple files

---
+++ src/gridpts.h
@@ -0,0 +1,8 @@
+#ifndef GSDESIGN2_SRC_GRIDPTS_H
+#define GSDESIGN2_SRC_GRIDPTS_H
+
+#include <Rcpp.h>
+
+Rcpp::List gridpts_rcpp(int r, double mu, double a, double b);
+
+#endif // GSDESIGN2_SRC_GRIDPTS_H
