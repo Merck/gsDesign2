9806efa18b855ae457c318281fc6cbfc6a1b487e
Nan Xiao <nan.xiao1@merck.com>
2024-03-18 12:17:30 -0400

    Style `ahr_blinded()` documentation and code

--- R/ahr_blinded.R
+++ R/ahr_blinded.R
@@ -1,3 +1,6 @@
+#  Copyright (c) 2024 Merck & Co., Inc., Rahway, NJ, USA and its affiliates.
+#  All rights reserved.
+#
 #  This file is part of the gsDesign2 program.
 #
 #  gsDesign2 is free software: you can redistribute it and/or modify
@@ -15,49 +18,55 @@
 
 #' Blinded estimation of average hazard ratio
 #'
-#' Based on blinded data and assumed hazard ratios in different intervals, compute
-#' a blinded estimate of average hazard ratio (AHR) and corresponding estimate of statistical information.
-#' This function is intended for use in computing futility bounds based on spending assuming
-#' the input hazard ratio (hr) values for intervals specified here.
-#' @importFrom tibble tibble
-#' @importFrom survival Surv
-#' @param surv input survival object (see \code{Surv}); note that only 0=censored, 1=event for \code{Surv}
-#' @param intervals Vector containing positive values indicating interval lengths where the
-#' exponential rates are assumed.
-#' Note that a final infinite interval is added if any events occur after the final interval
-#' specified.
-#' @param hr vector of hazard ratios assumed for each interval
-#' @param ratio ratio of experimental to control randomization.
+#' Based on blinded data and assumed hazard ratios in different intervals,
+#' compute a blinded estimate of average hazard ratio (AHR) and corresponding
+#' estimate of statistical information.
+#' This function is intended for use in computing futility bounds based on
+#' spending assuming the input hazard ratio (hr) values for intervals
+#' specified here.
+#'
+#' @param surv Input survival object (see [survival::Surv()]);
+#'   note that only 0 = censored, 1 = event for [survival::Surv()].
+#' @param intervals Vector containing positive values indicating
+#'   interval lengths where the exponential rates are assumed.
+#'   Note that a final infinite interval is added if any events occur
+#'   after the final interval specified.
+#' @param hr Vector of hazard ratios assumed for each interval.
+#' @param ratio Ratio of experimental to control randomization.
+#'
+#' @return A `tibble` with one row containing
+#' - `ahr` - Blinded average hazard ratio based on assumed period-specific
+#'   hazard ratios input in `fail_rate` and observed events in the
+#'   corresponding intervals.
+#' - `event` - Total observed number of events.
+#' - `info0` - Information under related null hypothesis.
 #'
 #' @section Specification:
 #' \if{latex}{
 #'  \itemize{
 #'    \item Validate if input hr is a numeric vector.
 #'    \item Validate if input hr is non-negative.
-#'    \item Simulate piece-wise exponential survival estimation with the inputs survival object surv
-#'    and intervals.
-#'    \item Save the length of  hr and events to an object, and if the length of hr is shorter than
-#'    the intervals, add replicates of the last element of hr and the corresponding numbers of events
-#'    to hr.
+#'    \item Simulate piece-wise exponential survival estimation with the inputs
+#'    survival object surv and intervals.
+#'    \item Save the length of  hr and events to an object, and if the length of
+#'    hr is shorter than the intervals, add replicates of the last element of hr
+#'    and the corresponding numbers of events to hr.
 #'    \item Compute the blinded estimation of average hazard ratio.
 #'    \item Compute adjustment for information.
-#'    \item Return a tibble of the sum of events, average hazard raito, blinded average hazard
-#'    ratio, and the information.
+#'    \item Return a tibble of the sum of events, average hazard ratio,
+#'    blinded average hazard ratio, and the information.
 #'   }
 #' }
 #' \if{html}{The contents of this section are shown in PDF user manual only.}
 #'
-#' @return A \code{tibble} with one row containing
-#' `ahr` blinded average hazard ratio based on assumed period-specific hazard ratios input in `fail_rate`
-#' and observed events in the corresponding intervals
-#' `event` total observed number of events, and `info0` (information under related null hypothesis).
+#' @importFrom tibble tibble
+#' @importFrom survival Surv
+#'
+#' @export
 #'
 #' @examples
-#' \donttest{
-#' library(simtrial)
-#' library(survival)
 #' ahr_blinded(
-#'   surv = Surv(
+#'   surv = survival::Surv(
 #'     time = simtrial::ex2_delayed_effect$month,
 #'     event = simtrial::ex2_delayed_effect$evntd
 #'   ),
@@ -65,14 +74,14 @@
 #'   hr = c(1, .55),
 #'   ratio = 1
 #' )
-#' }
-#'
-#' @export
-ahr_blinded <- function(surv = survival::Surv(time = simtrial::ex1_delayed_effect$month,
-                                              event = simtrial::ex1_delayed_effect$evntd),
-                        intervals = c(3, Inf),
-                        hr = c(1, .6),
-                        ratio = 1) {
+ahr_blinded <- function(
+    surv = survival::Surv(
+      time = simtrial::ex1_delayed_effect$month,
+      event = simtrial::ex1_delayed_effect$evntd
+    ),
+    intervals = c(3, Inf),
+    hr = c(1, .6),
+    ratio = 1) {
   # Input checking
   if (!is.vector(hr, mode = "numeric") || min(hr) <= 0) {
     stop("ahr_blinded: hr must be a vector of positive numbers.")
@@ -94,19 +103,19 @@ ahr_blinded <- function(surv = survival::Surv(time = simtrial::ex1_delayed_effec
   nx <- length(event)
 
   # Add to hr if length shorter than intervals
-  if (length(hr) < length(event)) {
-    hr <- c(hr, rep(hr[nhr], nx - nhr))
-  }
+  if (length(hr) < length(event)) hr <- c(hr, rep(hr[nhr], nx - nhr))
 
   # Compute blinded AHR
-  theta <- - sum(log(hr[1:nx]) * event) / sum(event)
+  theta <- -sum(log(hr[1:nx]) * event) / sum(event)
 
   # Compute adjustment for information
   q_e <- ratio / (1 + ratio)
 
   ans <- tibble(
-    event = sum(event), ahr = exp(-theta),
-    theta = theta, info0 = sum(event) * (1 - q_e) * q_e
+    event = sum(event),
+    ahr = exp(-theta),
+    theta = theta,
+    info0 = sum(event) * (1 - q_e) * q_e
   )
   return(ans)
 }
