130d38ac78d51ae5baffa8ab28e738c9d2fff571
Nan Xiao <me@nanx.me>
2024-04-15 20:45:23 -0400

    Restore the original method matching implementation

--- R/as_rtf.R
+++ R/as_rtf.R
@@ -414,15 +414,7 @@ as_rtf.gs_design <- function(
     ...) {
   orientation <- match.arg(orientation)
 
-  #method <- class(x)[class(x) %in% c("ahr", "wlr", "combo", "rd")]
-  method <- switch(
-    class(x),
-    "ahr",
-    "wlr",
-    "combo",
-    "rd",
-    "Unknown method"
-  )
+  method <- class(x)[class(x) %in% c("ahr", "wlr", "combo", "rd")]
   x_alpha <- max((x %>% dplyr::filter(Bound == display_bound[1]))[["Null hypothesis"]])
   x_non_binding <- "non_binding" %in% class(x)
   x_k <- lapply(x$Analysis, function(x) {
