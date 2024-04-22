24af93d4d4eaaa80878f71e47a3eae60be3b4454
anderkea <anderkea@lue1vc20051.merck.com>
2024-04-22 07:01:16 -0400

    fixed intervals issue

--- R/ahr_blinded.R
+++ R/ahr_blinded.R
@@ -48,10 +48,10 @@
 #'  \itemize{
 #'    \item Validate input hr is a numeric vector.
 #'    \item Validate input hr is non-negative.
+#'    \item Validate input intervals is a numeric vector > 0.
+#'    \item Set final value in intervals to Inf
+#'    \item Validate that hr and intervals have same length.
 #'    \item For input time-to-event data, count number of events in each input interval by stratum.
-#'    \item Save the length of hr and events to an object, and if the length of
-#'    hr is shorter than the intervals, add replicate of the last element of hr
-#'    and the corresponding numbers of events to hr.
 #'    \item Compute the blinded estimate of average hazard ratio.
 #'    \item Compute adjustment for information.
 #'    \item Return a tibble of the sum of events, average hazard ratio,
@@ -87,16 +87,15 @@ ahr_blinded <- function(
   if (!is.vector(hr, mode = "numeric") || min(hr) <= 0) {
     stop("ahr_blinded: hr must be a vector of positive numbers.")
   }
-
-  tte_data <- data.frame(time = surv[, "time"], status = surv[, "status"])
-  if (nrow(subset(tte_data, time > sum(intervals) & status > 0)) > 0) {
-    intervals_imputed <- c(intervals, Inf)
-  } else {
-    intervals_imputed <- intervals
+  if (!is.vector(intervals, mode = "numeric") || min(intervals) <= 0) {
+    stop("ahr_blinded: intervals must be a vector of positive numbers.")
   }
-  if (length(intervals_imputed) != length(hr)) {
+  if (length(intervals) != length(hr)) {
     stop("ahr_blinded: the piecewise model specified hr and intervals are not aligned.")
   }
+
+  # Set final element of "intervals" to Inf
+  intervals[length(intervals)] <- Inf
 
   # Fit the survival data into piecewise exponential model
   event <- simtrial::fit_pwexp(surv, intervals)[, 3]
