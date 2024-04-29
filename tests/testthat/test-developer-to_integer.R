af9b7f1ea453e58ca782828a2506e6f8ab872867
Zhao, Yujie <yujie.zhao@merck.com>
2024-04-29 13:39:19 -0400

    lintr

--- tests/testthat/test-developer-to_integer.R
+++ tests/testthat/test-developer-to_integer.R
@@ -7,8 +7,7 @@ test_that("The IA nominal p-value is the same as the IA alpha spending.", {
                 param = NULL,
                 timing = c(18, 30) / 30),
     lower = gs_b,
-    lpar = c(-Inf, -Inf)
-    )|>
+    lpar = c(-Inf, -Inf)) |>
     to_integer()
 
   expect_equal(x$bound$`nominal p`[1], gsDesign::sfLDOF(alpha = 0.025, t = 18 / 30)$spend[1])
