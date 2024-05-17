3f1f5da211bd188addd9b57e8a8f4327718b447c
Zhao, Yujie <yujie.zhao@merck.com>
2024-05-17 17:11:41 -0400

    fix testcoverge error

--- tests/testthat/test-independent_as_gt.R
+++ tests/testthat/test-independent_as_gt.R
@@ -124,7 +124,7 @@ test_that("Snapshot test for gs_design_ahr summary as_gt", {
 test_that("Snapshot test for gs_power_ahr summary as_gt", {
   skip_on_cran()
 
-  output <- gs_power_ahr() %>%
+  output <- gs_power_ahr(lpar = list(sf = gsDesign::sfLDOF, total_spend = 0.1)) %>%
     summary() %>%
     as_gt()
 
@@ -146,7 +146,7 @@ test_that("Snapshot test for gs_design_wlr summary as_gt", {
 test_that("Snapshot test for gs_power_wlr summary as_gt", {
   skip_on_cran()
 
-  output <- gs_power_wlr() %>%
+  output <- gs_power_wlr(lpar = list(sf = gsDesign::sfLDOF, total_spend = 0.1)) %>%
     summary() %>%
     as_gt(
       footnote = list(
@@ -215,7 +215,7 @@ test_that("Snapshot test for gs_power_rd summary as_gt", {
 test_that("Snapshot test for gs_power_wlr summary as_gt with custom title and subtitle", {
   skip_on_cran()
 
-  output <- gs_power_wlr() %>%
+  output <- gs_power_wlr(lpar = list(sf = gsDesign::sfLDOF, total_spend = 0.1)) %>%
     summary() %>%
     as_gt(title = "Bound Summary", subtitle = "from gs_power_wlr")
 
@@ -226,7 +226,7 @@ test_that("Snapshot test for gs_power_wlr summary as_gt with custom title and su
 test_that("Snapshot test for gs_power_wlr summary as_gt with colname_spanner and colname_spannersub", {
   skip_on_cran()
 
-  output <- gs_power_wlr() %>%
+  output <- gs_power_wlr(lpar = list(sf = gsDesign::sfLDOF, total_spend = 0.1)) %>%
     summary() %>%
     as_gt(
       colname_spanner = "Cumulative probability to cross boundaries",
@@ -241,7 +241,7 @@ test_that("Snapshot test for gs_power_wlr summary as_gt with colname_spanner and
 test_that("Snapshot test for gs_power_wlr summary as_gt with custom footnotes", {
   skip_on_cran()
 
-  output <- gs_power_wlr() %>%
+  output <- gs_power_wlr(lpar = list(sf = gsDesign::sfLDOF, total_spend = 0.1)) %>%
     summary() %>%
     as_gt(
       footnote = list(
@@ -263,7 +263,7 @@ test_that("Snapshot test for gs_power_wlr summary as_gt with custom footnotes", 
 test_that("Snapshot test for gs_power_wlr summary as_gt with display_bound", {
   skip_on_cran()
 
-  output <- gs_power_wlr() %>%
+  output <- gs_power_wlr(lpar = list(sf = gsDesign::sfLDOF, total_spend = 0.1)) %>%
     summary() %>%
     as_gt(display_bound = "Efficacy")
 
@@ -274,7 +274,7 @@ test_that("Snapshot test for gs_power_wlr summary as_gt with display_bound", {
 test_that("Snapshot test for gs_power_wlr summary as_gt with display_columns", {
   skip_on_cran()
 
-  output <- gs_power_wlr() %>%
+  output <- gs_power_wlr(lpar = list(sf = gsDesign::sfLDOF, total_spend = 0.1)) %>%
     summary() %>%
     as_gt(display_columns = c("Analysis", "Bound", "Nominal p", "Z", "Probability"))
 
