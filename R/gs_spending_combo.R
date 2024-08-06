69a1f550af55d071ec5d8a532de7277076ec1d6e
Zhao, Yujie <yujie.zhao@merck.com>
2024-08-06 08:41:37 -0400

    delete `...`

--- R/gs_spending_combo.R
+++ R/gs_spending_combo.R
@@ -19,7 +19,6 @@
 #' Derive spending bound for MaxCombo group sequential boundary
 #'
 #' @inheritParams gs_spending_bound
-#' @param ... Additional parameters passed to `par$sf`.
 #'
 #' @return A vector of the alpha spending per analysis.
 #'
@@ -101,6 +100,6 @@
 #' # beta-spending
 #' par <- list(sf = gsDesign::sfLDOF, total_spend = 0.2)
 #' gs_spending_combo(par, info = 1:3 / 3)
-gs_spending_combo <- function(par = NULL, info = NULL, ...) {
+gs_spending_combo <- function(par = NULL, info = NULL) {
   par$sf(alpha = par$total_spend, t = info, param = par$param, ...)$spend
 }
