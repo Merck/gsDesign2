16ccda7618b9c2dc4a2366f58212c5f7088f5719
John Blischak <jdblischak@gmail.com>
2025-04-24 11:45:02 -0400

    Add arg `alpha` to `gs_bound_summary()` to report multiple efficacy bounds

--- R/gs_bound_summary.R
+++ R/gs_bound_summary.R
@@ -3,6 +3,7 @@
 #' Summarizes the efficacy and futility bounds for each analysis.
 #'
 #' @param x design object
+#' @param alpha vector of alpha values to compute additional efficacy columns
 #'
 #' @return A data frame
 #'
@@ -17,8 +18,36 @@
 #' x <- gs_design_wlr(info_frac = c(.25, .75, 1), analysis_time = c(12, 25, 36))
 #' gs_bound_summary(x)
 #'
+#' # Report multiple efficacy bounds (only supported for AHR designs)
+#' x <- gs_design_ahr(analysis_time = 1:3*12, alpha = 0.0125)
+#' gs_bound_summary(x, alpha = c(0.025, 0.05))
+#'
 #' @export
-gs_bound_summary <- function(x) {
+gs_bound_summary <- function(x, alpha = NULL) {
+  if (is.null(alpha)) return(gs_bound_summary_single(x))
+  if (!inherits(x, "ahr")) stop("The argument `alpha` is only supported for AHR design objects")
+  if (!is.numeric(alpha)) stop("The argument `alpha` must be a numeric vector")
+
+  # Support multiple alphas
+  alpha_original <- x[["input"]][["alpha"]]
+  alpha_new <- unique(sort(c(alpha, alpha_original)))
+  col_efficacy_name <- paste0("α=", alpha_new)
+  outlist <- vector("list", length = length(alpha_new))
+  for (i in seq_along(alpha_new)) {
+    if (alpha_new[i] == alpha_original) {
+      outlist[[i]] <- gs_bound_summary_single(x, col_efficacy_name[i])
+    } else {
+      x_updated <- gs_update_ahr(x, alpha = alpha_new[i])
+      x_updated_bounds <- gs_bound_summary_single(x_updated, col_efficacy_name[i])
+      outlist[[i]] <- x_updated_bounds[col_efficacy_name[i]]
+    }
+  }
+  out <- Reduce(cbind, outlist)
+  out <- out[, c("Analysis", "Value", col_efficacy_name, "Futility")]
+  return(out)
+}
+
+gs_bound_summary_single <- function(x, col_efficacy_name = "Efficacy") {
   # Input
   analysis <- x$analysis
   bound <- x$bound
@@ -79,13 +108,15 @@ gs_bound_summary <- function(x) {
     col_futility <- c(col_futility, as.numeric(row_futility))
   }
 
-  col_futility <- round(col_futility, 4)
   col_efficacy <- round(col_efficacy, 4)
+  col_futility <- round(col_futility, 4)
 
-  data.frame(
+  out <- data.frame(
     Analysis = col_analysis,
     Value = col_value,
     Efficacy = col_efficacy,
     Futility = col_futility
   )
+  colnames(out)[3] <- col_efficacy_name
+  return(out)
 }
