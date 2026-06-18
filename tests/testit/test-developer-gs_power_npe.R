55bbe269de61a4c172802763e8ce15f99665b390
LittleBeannie <yujie.zhao@merck.com>
2026-06-18 16:11:14 -0400

    Add more developer tests

--- tests/testit/test-developer-gs_power_npe.R
+++ tests/testit/test-developer-gs_power_npe.R
@@ -350,7 +350,9 @@ assert("Harm bound - Cap harm bound at futility bound", {
     hpar = list(sf = gsDesign::sfHSD, total_spend = 0.1, param = -4, timing = NULL),
     test_harm = c(TRUE, TRUE, TRUE)
   )
+  harm_analysis <- x |> dplyr::filter(bound == "harm") |> dplyr::pull(analysis)
   harm_bound <- x |> dplyr::filter(bound == "harm") |> dplyr::pull(z)
-  futility_bound <- x |> dplyr::filter(bound == "lower") |> dplyr::pull(z)
+  futility_bound <- x |> dplyr::filter(bound == "lower", analysis %in% harm_analysis) |> dplyr::pull(z)
   (harm_bound <= futility_bound)
+  (harm_analysis %==% 1:2)
 })
